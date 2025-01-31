1b11b935d682b1f4c173b66161491dcf0634e187
Shohruhbek Valiyev <s35305344@gmail.com>
2025-01-31 15:33:12 +0300

    saasa

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("hello world !!!")
-console.log("hello world !!!")
+let a = 86399
+let c = 0 
+let d = 0
+let e = 0
+let r =0
+
+c = a % 60
+d = (a / 60) % 60
+e = (a / 3600) % 24
+r = a / 86400
+console.log(r ,"kun",e ,"soat",d,"minut",c,"sekund")
